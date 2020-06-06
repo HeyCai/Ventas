d24bb1634075948eb2ce3ed2d5650f5d9f930b42
saavedrahenry5 <saavedrahenry5@gmail.com>
2020-06-06 20:22:04 +0200

    CRUDS mvc and Api, some mvvm

---
+++ Ventas/Ventas/Infrastructure/IntanceLocator.cs
@@ -0,0 +1,13 @@
+﻿namespace Ventas.Infrastructure
+{
+    using ViewModels;
+
+    public class IntanceLocator
+    {
+        public MainViewModel Main { get; set; }
+        public IntanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
